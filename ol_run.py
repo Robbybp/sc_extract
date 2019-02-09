1cd68b77fdaa599fa573e48dceec925a1b4e19ac
Robert Parker <robbybparker@gmail.com>
2019-02-08 22:55:24 -0500

    Added capability to initialize dynamic model from steady state.

--- ol_run.py
+++ ol_run.py
@@ -1,5 +1,6 @@
 from pyomo.environ import * 
 from pyomo.dae import ContinuousSet, DerivativeVar
+from pyomo.core.base.sets import _SetProduct
 import numpy as np
 from ss_mod import m as m_ss 
 from ol_mod import m
@@ -74,13 +75,98 @@ for t in m.t:
 
 m = alg_update(m,0)
 
+ol_xE = getattr(m,'xE_IPA')
+print(dir(ol_xE))
+print(dir(ol_xE.index_set()))
+print([v for v in ol_xE.index_set()])
+print(ol_xE.index_set().set_tuple)
+print(m.t in ol_xE.index_set().set_tuple)
+
+#print([v for _, v in ol_xE.index_set()])
+#print(m.t in ol_xE.index_set())
+#ol_K = getattr(m,'KF_CO2')
+#ol_F = getattr(m,'FFE')
+#print(ol_F)
+#print(ol_F.index_set())
+#print(type(ol_F.index_set()))
+#print(ol_K)
+#print(ol_K.index_set())
+#print([v for v in ol_K.index_set()])
+#print([t for t in m.t])
+# Copy steady state initialization for all time
+for var_ss in m_ss.component_objects(Var, active=True):
+    print(var_ss)
+#    print(type(var_ss))
+#    print(str(var_ss))
+#    print(type(str(var_ss)))
+    #print(var_ss.value)
+    var_name=str(var_ss)
+    var_ol = getattr(m,var_name)
+    if type(var_ss.index_set()) is _SetProduct:
+        ss_index_sets = var_ss.index_set().set_tuple
+    else:
+        ss_index_sets = var_ss.index_set()
+#    print(ss_index_sets)
+    if type(var_ol.index_set()) is _SetProduct:
+        ol_index_sets = var_ol.index_set().set_tuple
+    else: 
+        ol_index_sets = var_ol.index_set() 
+
+    print(ol_index_sets)
+    print('t in set? ',m.t in ol_index_sets)
+    print('t is idx? ',m.t == ol_index_sets)
+
+    print('var_ol: \t',var_ol)
+    for index in var_ss:
+        print(index)
+        var_ss_value = var_ss[index].value
+        index_type = type(index)
+        if index is None:
+            if m.t in ol_index_sets:
+
+                for t in m.t:
+                    ol_index = t
+                    var_ol[ol_index].set_value(var_ss_value)
+                print(ol_index)
+            else: 
+                var_ol.set_value(var_ss_value)
+            continue 
+        if index_type is int:
+            for t in m.t:
+                ol_index = (index,t)
+                var_ol[ol_index].set_value(var_ss_value)
+            print(ol_index)
+            continue
+        if index_type is tuple:
+            for t in m.t:
+                ol_index = index + (t) 
+                var_ol[ol_index].set_value(var_ss_value)
+            print(ol_index)
+            continue
+
+#        print(type(index))
+#        print(var_ss[index])
+
+
+#print(getattr(m_ss,'xE_IPA'))
+
+#for index in var_ol:
+#    print(index)
+#    print(type(index))
+#
+#tuple1 = (1,2)
+#print(tuple(tuple1))
+#three = tuple(3)
+#tuple2 = (tuple1,3)
+#print(tuple2)
+
 with open('m_ol_init.txt','w') as f:
     m.display(ostream=f)
-
+#
 ipopt = SolverFactory('ipopt')
 ipopt.solve(m,tee=True)
 
-#m_ol.write('ol_mod.nl')#,io_options={symbolic_solver_labels:True})
+m.write('ol_mod.nl')#,io_options={symbolic_solver_labels:True})
 
 
 with open('m_ol.txt', 'w') as f:
